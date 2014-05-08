08321748db1c6540e023b00fb9241ec232074107
Primoz Cigler <primoz@proteusnet.com>
2014-05-08 12:40:56 +0200

    inited the repo, created with wp-cli scaffold tool

---
+++ remove-css-ids.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Remove CSS link IDs
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: remove-css-ids
+Domain Path: /languages
+*/
+
